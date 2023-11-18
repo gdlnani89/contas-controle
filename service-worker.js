987a3ef7ac407c21b3780f1fd284939fb995e3ba
Guilherme <gdlnani.89@gmail.com>
2023-11-18 15:52:31 -0300

    pwa

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,28 @@
-const cacheName = 'my-app-cache';
+const cacheVersion = 'v1';
+const cacheName = `my-app-cache-${cacheVersion}`;
 
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(cacheName).then(cache => {
       return cache.addAll([
         'index.html',
-        '/css/styles.css',
+        '/css/style.css',
         '/js/index.js',
         '/js/vue.min.js'
       ]);
-    }).catch(error =>{
-        console.error('Falha ao armazenar em cache recursos:', error)
+    }).catch(error => {
+      console.error('Falha ao armazenar em cache recursos:', error);
+    })
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(keys
+        .filter(key => key !== cacheName)
+        .map(key => caches.delete(key))
+      );
     })
   );
 });
